3b1a65d5969a36c22b2ba4571e47b8d380161551
Marjorie Andrieux <andrieux.marjoriebeatricelydie@gmail.com>
2018-03-16 12:39:05 +0100

    Ajout fonction js

---
+++ js/anim.js
@@ -0,0 +1,15 @@
+$("#reservation1").click(function(){
+
+var recup=$("#hotel1").text();
+$("#exampleModalLabel").html(recup);
+});
+
+$("#reservation2").click(function(){
+var recup=$("#hotel2").text();
+$("#exampleModalLabel").html(recup);
+});
+
+$("#reservation3").click(function(){
+var recup=$("#hotel3").text();
+$("#exampleModalLabel").html(recup);
+});
